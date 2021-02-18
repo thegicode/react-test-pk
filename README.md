88c090a9b7b824a19515d702f1445b6b17e60773
thegicode <hdi@gimdeog-im-ui-MacBook.local>
2021-02-18 14:08:28 +0900

    Add Order

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # react-test-pk
 react test
+
+# Babel
+npx babel --watch src --out-dir lib --presets react-app/prod
---
+++ src/order.js
@@ -0,0 +1,71 @@
+'use strict';
+
+
+const useState = React.useState;
+const el = React.createElement;
+
+function Order(){
+  const [isShowPopup, setIsShowPopup] = useState(false);
+  const [data, setData] = useState({});
+
+  function openPopup(){
+    // window.history.pushState('popup-addr', '', '/popup-addr');
+    setIsShowPopup( true );
+  }
+
+  const closePopup = (data) => {
+    // window.history.back();
+    setIsShowPopup( false );
+    setData(data);
+  };
+
+  // window.onpopstate = (event) => {
+  //   setIsShowPopup( false );
+  // }
+
+  return(
+    <React.Fragment>
+
+      <h2>Order</h2>
+
+      { Object.keys(data).length > 0  && 
+        <div className="order-data">
+          <p>{data.addr}</p>
+          <p>{data.mobile}</p>
+        </div>
+      }
+
+      <button onClick={openPopup}>Show Popup</button>
+
+      { isShowPopup && 
+        <PopupContent onCloseButtonClick = {closePopup} />
+      }
+
+    </React.Fragment>
+  )
+
+}
+
+function PopupContent(props) {
+
+  const data = {
+    addr: '서울시 강동구 고덕동',
+    mobile: '010-124-6790'
+  };
+
+  function handleClick(){
+    props.onCloseButtonClick(data);
+  }
+
+  return (
+    <div className="popupContent">
+      <button type="button" onClick={handleClick}>Popup Close & Pass data</button>
+    </div>
+  );
+}
+
+
+const domContainer = document.querySelector('#order');
+ReactDOM.render(el(Order), domContainer);
+
+
